2cfafb146a4d499f66980bb7833a6ebb9aa5be07
jhoncabrera2050 <jhoncabrera2050@gmail.com>
2023-09-01 16:33:29 -0500

    actualizando

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -7,7 +7,6 @@ import {
   Stack,
   Text,
   useDisclosure,
-  VStack,
 } from "@chakra-ui/react"
 
 const Experience = () => {
@@ -16,31 +15,33 @@ const Experience = () => {
   return (
     <Box mt={10}>
       <Heading size='lg'>Experience</Heading>
-      <HStack>
-        <Stack
-          bg='#FFF'
-          maxW={"350px"}
-          borderWidth='1px'
-          rounded='md'
-          my={'5'}
-          p={'2'}
-          _hover={{
-            boxShadow: 'rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px',
-          }}
-        >
-          <HStack align='flex-start' onClick={onToggle}>
-            <Image
-              boxSize={'55px'}
-              src={'agencia.jpg'}
-              alt="yanbal"
-            />
-            <Box>
-              <Text fontWeight='semibold'>Maquetador Web</Text>
-              <Text fontSize='15px'>
-                Agencia Online JF
-              </Text>
-            </Box>
-            {/* <Collapse in={isOpen} animateOpacity>
+      <Stack
+        bg='#FFF'
+        maxW={"350px"}
+        borderWidth='1px'
+        rounded='md'
+        my={'5'}
+        p={'2'}
+        _hover={{
+          boxShadow: 'rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px',
+        }}
+      >
+        <HStack align='flex-start' onClick={onToggle}>
+          <Image
+            boxSize={'55px'}
+            src={'agencia.jpg'}
+            alt="atento"
+          />
+          <Box>
+            <Text fontWeight='semibold'>Maquetador Web</Text>
+            <Text fontSize='15px'>
+              Agencia Online JF
+            </Text>
+            <Text fontSize='15px'>
+              Junio. 2023 - Septiembre · 2023
+            </Text>
+          </Box>
+          {/* <Collapse in={isOpen} animateOpacity>
             <Box
               p='40px'
               bg={"#181818"}
@@ -53,32 +54,35 @@ const Experience = () => {
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum eveniet laborum animi vel quod omnis perferendis ipsum! Voluptatum enim necessitatibus tempore minima doloribus repudiandae! Dignissimos voluptate facilis a ducimus earum.
             </Box>
           </Collapse> */}
-          </HStack>
-        </Stack>
-        <Stack
-          bg='#FFF'
-          maxW={"350px"}
-          borderWidth='1px'
-          rounded='md'
-          my={'5'}
-          p={'2'}
-          _hover={{
-            boxShadow: 'rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px',
-          }}
-        >
-          <HStack align='flex-start' onClick={onToggle}>
-            <Image
-              boxSize={'55px'}
-              src={'yanbal.jpg'}
-              alt="atento"
-            />
-            <Box>
-              <Text fontWeight='semibold'>Pasante Programador</Text>
-              <Text fontSize='15px'>
-                Yanbal Unique
-              </Text>
-            </Box>
-            {/* <Collapse in={isOpen} animateOpacity>
+        </HStack>
+      </Stack>
+      <Stack
+        bg='#FFF'
+        maxW={"350px"}
+        borderWidth='1px'
+        rounded='md'
+        my={'5'}
+        p={'2'}
+        _hover={{
+          boxShadow: 'rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px',
+        }}
+      >
+        <HStack align='flex-start' onClick={onToggle}>
+          <Image
+            boxSize={'55px'}
+            src={'yanbal.jpg'}
+            alt="yanbal"
+          />
+          <Box>
+            <Text fontWeight='semibold'>Programador Junior</Text>
+            <Text fontSize='15px'>
+              Yanbal
+            </Text>
+            <Text fontSize='15px'>
+              ene. 2023 - Marzo · 3 meses
+            </Text>
+          </Box>
+          {/* <Collapse in={isOpen} animateOpacity>
             <Box
               p='40px'
               bg={"#181818"}
@@ -91,32 +95,35 @@ const Experience = () => {
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum eveniet laborum animi vel quod omnis perferendis ipsum! Voluptatum enim necessitatibus tempore minima doloribus repudiandae! Dignissimos voluptate facilis a ducimus earum.
             </Box>
           </Collapse> */}
-          </HStack>
-        </Stack>
-        <Stack
-          bg='#FFF'
-          maxW={"350px"}
-          borderWidth='1px'
-          rounded='md'
-          my={'5'}
-          p={'2'}
-          _hover={{
-            boxShadow: 'rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px',
-          }}
-        >
-          <HStack align='flex-start' onClick={onToggle}>
-            <Image
-              boxSize={'55px'}
-              src={'atento.jpg'}
-              alt="atento"
-            />
-            <Box>
-              <Text fontWeight='semibold'>Asesor Técnico</Text>
-              <Text fontSize='15px'>
-                Atento
-              </Text>
-            </Box>
-            {/* <Collapse in={isOpen} animateOpacity>
+        </HStack>
+      </Stack>
+      <Stack
+        bg='#FFF'
+        maxW={"350px"}
+        borderWidth='1px'
+        rounded='md'
+        my={'5'}
+        p={'2'}
+        _hover={{
+          boxShadow: 'rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px',
+        }}
+      >
+        <HStack align='flex-start' onClick={onToggle}>
+          <Image
+            boxSize={'55px'}
+            src={'atento.jpg'}
+            alt="atento"
+          />
+          <Box>
+            <Text fontWeight='semibold'>Asesor de Servicio</Text>
+            <Text fontSize='15px'>
+              Atento
+            </Text>
+            <Text fontSize='15px'>
+              ene. 2022 - Enero · 2023 1 año 
+            </Text>
+          </Box>
+          {/* <Collapse in={isOpen} animateOpacity>
             <Box
               p='40px'
               bg={"#181818"}
@@ -129,11 +136,10 @@ const Experience = () => {
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum eveniet laborum animi vel quod omnis perferendis ipsum! Voluptatum enim necessitatibus tempore minima doloribus repudiandae! Dignissimos voluptate facilis a ducimus earum.
             </Box>
           </Collapse> */}
-          </HStack>
-        </Stack>
-      </HStack>
+        </HStack>
+      </Stack>
     </Box>
-
+    
   )
 }
 
